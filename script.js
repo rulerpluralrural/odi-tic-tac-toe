f82179074f40a82ff4856395570ca267f1c271af
Gerald Javier <33073866+rulerpluralrural@users.noreply.github.com>
2023-07-22 22:49:26 +0800

    " Added display controller function "
    * Menu button will determine players marker
    * Cells clicked will display marker
    * Text shows players turn

--- script.js
+++ script.js
@@ -1,17 +1,73 @@
-// Close start menu
-    const closeBtn = document.getElementById('close')
-    const menu = document.getElementById('menu-options')
 
-    closeBtn.addEventListener('click', () => {
-        menu.classList.add('close')
-    })
-
-function players(player, hand) {
-    return {
-        player:player,
-        hand:hand
-    }
+const players = (marker) => {
+    return {marker}
 }
 
-let playerOne = players('player one', 'X')
-let playerTwo = players('player two', 'O')
+
+const gameBoard = (() => {
+    let board = ['', '', '', '', '', '', '', '', '']
+
+    const setBoard = (index, marker) => {
+        if (index > board.length) return;
+        board[index] = marker
+    }
+
+    const getBoard = (index) => {
+        if (index > board.length) return;
+        return board[index] 
+    }
+
+    return{getBoard, setBoard}
+})();
+
+// Display the marker on the board
+const displayController = (() => {
+
+    const displayResult = document.getElementById('result')
+    const displayTurn = document.getElementById('turn')
+    const cards = document.querySelectorAll('.card')
+
+    cards.forEach(card => card.addEventListener('click', (e) => {
+
+            if(e.target.innerText !== '') {
+                return
+            } else if(e.target.innerText === '') {
+                e.target.innerText = players.marker
+                e.target.classList.remove('pointer')
+                e.target.classList.add('not-allowed')
+                e.target.removeEventListener('click', e.target)
+            }
+
+            if(players.marker === 'X') {
+                players.marker = 'O'
+                displayTurn.textContent = `It\'s ${players.marker}\'s turn`
+            } else if(players.marker === 'O') {
+                players.marker = 'X'
+                displayTurn.textContent =`It\'s ${players.marker}\'s turn`
+            }
+        }))
+        
+})();
+
+// Get the players marker on button click
+const getPlayerMarker = (() => {
+
+    const displayTurn = document.getElementById('turn')
+    const xMarker = document.getElementById('x-marker')
+    const oMarker = document.getElementById('o-marker')
+    const menu = document.getElementById('menu-options')
+
+    xMarker.addEventListener('click', (e) => {
+        players.marker = e.target.textContent
+        menu.classList.add('close')
+        displayTurn.textContent = `${players.marker} goes first, It\'s ${players.marker}\'s turn`
+    })
+    oMarker.addEventListener('click', (e) => {
+        players.marker = 'X'
+        menu.classList.add('close')
+        displayTurn.textContent = `X goes first, It\'s X's turn`
+    })
+
+})();
+
+
